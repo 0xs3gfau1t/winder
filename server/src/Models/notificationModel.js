50aa05b3cfc13aeed7143a13feef0d8aa2f69172
Anish Chapagai <57314527+needl3@users.noreply.github.com>
2022-06-08 16:13:35 +0545

    use const instead of Object.freeze

--- server/src/Models/notificationModel.js
+++ server/src/Models/notificationModel.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
 // Enums for notification types
-NotificationTypes = {
+const NotificationTypes = {
 	LIKED:0,
 	MATCHED:1,
 	PROMOTIONS:2	
 };
-
-// Disables adding new enums
-Object.freeze(NotificationTypes);
 
 notificationSchema = mongoose.Schema({
 	type: Number,	// Use NotificationTypes during operations
