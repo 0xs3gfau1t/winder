1138e8197047fcaa526fca1a21a17a790370ef44
Bijan Regmi <vizandabijan@gmail.com>
2022-06-13 18:54:12 +0545

    moved socket io configs

--- server/src/Config/app.js
+++ server/src/Config/app.js
@@ -1,14 +1,21 @@
+// Express App
 const express = require("express")
-
 const app = express()
-
 // Configure middleware
 app.use(express.json())
+app.use(require("cookie-parser")())
 app.use(
 	express.urlencoded({
 		extended: true,
 	})
 )
-app.use(require('cookie-parser')())
 
-module.exports = app
+// HTTP SERVER
+const server = require("http").createServer(app)
+
+// SOCKET IO
+const io = require("socket.io")(server, { cors: { origin: "*" } })
+const { onConnectionHandler } = require("../Controllers/socket")
+io.on("connection", onConnectionHandler)
+
+module.exports = { app, io, server }
