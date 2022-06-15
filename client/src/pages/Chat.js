8c710276e866170d6ec26512f4a3d8e2e97accd1
paraspoudel72 <pas072bct020@wrc.edu.np>
2022-06-15 14:24:36 +0545

    made a frame of chat page

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/ChatPage'
+import ChatHead from '../components/ChatHead/ChatHead'
 import Nav from '../components/Nav/Nav'
 
 
-
 function Chat() {
+
+  const a = Array.from(Array(15).keys());
+
   return (
     <Wrapper>
         <div>
-        <h1>Chat</h1>
+          <h1>Chat</h1>
         <div className= "navbarr">
             <Nav current='Chat'/>
         </div>
+        <div className = 'chatsContainer'>
+          {a.map(element => {
+            return(<div className = 'chatContainer'>
+              <ChatHead/>
+            </div>)
+          })}
+          
+          {/* <div className = 'chatContainer'>
+            <ChatHead/>
+          </div>
+           <div className = 'chatContainer'>
+            <ChatHead/>
+          </div> */}
+        </div>
+       
     </div>
     </Wrapper>
   )
