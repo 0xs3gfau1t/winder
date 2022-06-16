58dc368acc5622d149d6e2c6346b58893a2396d8
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-06-16 23:14:56 +0545

    endpoint to get userdata and available options

--- server/src/Routes/User.js
+++ server/src/Routes/User.js
@@ -1,19 +1,28 @@
-require('dotenv').config()
+require("dotenv").config()
 
-const express = require('express');
+const express = require("express")
 
-const router = express.Router();
+const router = express.Router()
 
-const auth = require('../Middlewares/authenticateToken');
-const { userModel } = require('../Models/userModel');
+const auth = require("../Middlewares/authenticateToken")
+const { userModel } = require("../Models/userModel")
 
-router.get("/", auth, async (req, res)=>{
-    try {
-        const user = await userModel.find({_id: req.userdata._id})
-        res.json({success: true, user})
-    } catch (err) {
-        res.json({success: false, error: "Failed to retrieve user info."})
-    }
+const { options } = require("../Utils/variables")
+
+router.get("/options", (req, res) => {
+	res.json(options)
 })
 
-module.exports = router;
+router.get("/", auth, async (req, res) => {
+	try {
+		const user = await userModel.findOne(
+			{ _id: req.userdata._id },
+			{ password: 0, refreshToken: 0, pagination: 0 }
+		)
+		res.json({ success: true, user })
+	} catch (err) {
+		res.json({ success: false, error: "Failed to retrieve user info." })
+	}
+})
+
+module.exports = router
