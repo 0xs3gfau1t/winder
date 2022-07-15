8ca98b0305be672bbe418803c7a8d2a149a703d0
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-15 21:10:40 +0545

    Changing client url made easier.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,42 +1,45 @@
-const path = require('path')
-const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack');
+const path = require("path")
+const webpack = require("webpack")
+const Dotenv = require("dotenv-webpack")
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") })
+
+const host = process.env.HOST || "localhost"
+const port = process.env.PORT || "8000"
 
 module.exports = {
-    output: {
-        path: path.join(__dirname, '/static'),
-        filename: 'main.js',
-    },
-    devServer: {
-    static: {
-      directory: path.join(__dirname, '/src'),
-    },
-    compress: true,
-    port: 8000,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node-modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader","postcss-loader"],
-      },
-      {
-        test: /\.(svg|png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new Dotenv(),
-  ],
+	output: {
+		path: path.join(__dirname, "/static"),
+		filename: "main.js",
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "/src"),
+		},
+		compress: true,
+		host,
+		port,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(jsx|js)$/,
+				exclude: /node-modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(svg|png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: "file-loader",
+					},
+				],
+			},
+		],
+	},
+	plugins: [new Dotenv()],
 }
