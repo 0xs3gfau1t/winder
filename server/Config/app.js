dddaf54a3c84f3746636ddc0a286294342936be9
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-16 15:00:09 +0545

    add very simple logger

--- server/Config/app.js
+++ server/Config/app.js
@@ -4,6 +4,11 @@ const cors = require("cors")
 const session = require("express-session")
 const app = express()
 
+app.use(function (req, res, next){
+	console.log(`-> ${req.url}\n`)
+	next()
+})
+
 // Configure middleware
 app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true })) // Allow front end to access server's resources
 // https://stackoverflow.com/a/66553425/13001607
