7e5528a9ea90fa84dc483364d10fb570b7d41338
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-24 23:58:22 +0545

    Fix url typo

--- client/urls.js
+++ client/urls.js
@@ -5,7 +5,7 @@ export const EXPLORE_URL = "/api/explore"
 export const ACCEPT_URL = "/api/explore/accept"
 
 export const SETTINGS_URL = "/api/settings"
-export const VERIFY_MAIL_URL = "/api/settings/verifymail"
+export const VERIFY_MAIL_URL = "/api/settings/verifyemail"
 export const CHANGE_PW_URL = "/api/settings/changepassword"
 export const OPTIONS_URL = "/api/settings/options"
 
