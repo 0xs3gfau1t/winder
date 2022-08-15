0ac34611f5e1820e08da85a9bf0c22267cb5a089
needl3 <57314527+needl3@users.noreply.github.com>
2022-08-15 15:04:51 +0545

    HOST, PORT redefined to comply with containerized deployments

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const { NONAME } = require("dns")
 const path = require("path")
-require("dotenv").config({ path: path.resolve(__dirname, "./.env") })
 
 const { merge } = require("webpack-merge")
 const commonConfig = require("./webpack.common")
